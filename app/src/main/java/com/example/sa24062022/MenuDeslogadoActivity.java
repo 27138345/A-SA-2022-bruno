69adbf2769b0c5ad52c2b1df9b99bcda1e032161
Joaovs0 <joaovschoeller@gmail.com>
2022-07-12 11:25:45 -0300

    First Commit

---
+++ app/src/main/java/com/example/sa24062022/MenuDeslogadoActivity.java
@@ -0,0 +1,14 @@
+package com.example.sa24062022;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuDeslogadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_deslogado);
+    }
+}
---
+++ app/src/main/java/com/example/sa24062022/SobreActivity.java
@@ -0,0 +1,14 @@
+package com.example.sa24062022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
